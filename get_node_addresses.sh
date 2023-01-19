d1d565fb965ed4bebedfdaece42a79c09b597fd6
Clark Winters <github@clarkwinters.com>
2023-01-19 10:49:07 -0600

    allow passing a variable to get specific address info

--- get_node_addresses.sh
+++ get_node_addresses.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker network inspect edu | jq ".[0].Containers[]" | jq "{name: .Name, address: .IPv4Address}"
+docker network inspect edu | jq --arg name "$1" '.[0].Containers[] | if $name != "" then select(.Name == $name) else . end | {name: .Name, address: .IPv4Address}'
